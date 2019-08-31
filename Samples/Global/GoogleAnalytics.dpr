8a0c4310562ca8e16a3a126acdab1d4d96052a4c
Maicon da Silva Evangelista <maicon@omsistemas.com>
2019-08-31 19:18:42 -0300

    Ajustes na estrutura de arquivos

--- Samples/Global/GoogleAnalytics.dpr
+++ Samples/Global/GoogleAnalytics.dpr
@@ -3,19 +3,19 @@ program GoogleAnalytics;
 uses
   Vcl.Forms,
   Google.View.Principal in 'View\Google.View.Principal.pas' {frmPrincipal},
-  Google.Controller.Analytics.Interfaces in 'Controller\Google.Controller.Analytics.Interfaces.pas',
-  Google.Model.Analytics.Interfaces in 'Model\Google.Model.Analytics.Interfaces.pas',
-  Google.Controller.Analytics in 'Controller\Google.Controller.Analytics.pas',
-  Google.Model.Analytics.ScreenView in 'Model\Google.Model.Analytics.ScreenView.pas',
-  Google.Model.Analytics.Factory.Interfaces in 'Model\Google.Model.Analytics.Factory.Interfaces.pas',
-  Google.Model.Analytics.Factory in 'Model\Google.Model.Analytics.Factory.pas',
-  Google.Model.Analytics.AppInfo in 'Model\Google.Model.Analytics.AppInfo.pas',
-  Google.Model.Analytics.PageView in 'Model\Google.Model.Analytics.PageView.pas',
-  Google.Model.Analytics.Session in 'Model\Google.Model.Analytics.Session.pas',
-  Google.Model.Analytics.Event in 'Model\Google.Model.Analytics.Event.pas',
-  Google.Model.Analytics.Exception in 'Model\Google.Model.Analytics.Exception.pas',
-  Google.Model.Analytics.Invoker in 'Model\Google.Model.Analytics.Invoker.pas',
-  GoogleAnalyticsGlobal in 'GoogleAnalyticsGlobal.pas';
+  GoogleAnalyticsGlobal in 'GoogleAnalyticsGlobal.pas',
+  Google.Controller.Analytics.Interfaces in '..\..\Source\Controller\Google.Controller.Analytics.Interfaces.pas',
+  Google.Controller.Analytics in '..\..\Source\Controller\Google.Controller.Analytics.pas',
+  Google.Model.Analytics.AppInfo in '..\..\Source\Model\Google.Model.Analytics.AppInfo.pas',
+  Google.Model.Analytics.Event in '..\..\Source\Model\Google.Model.Analytics.Event.pas',
+  Google.Model.Analytics.Exception in '..\..\Source\Model\Google.Model.Analytics.Exception.pas',
+  Google.Model.Analytics.Factory.Interfaces in '..\..\Source\Model\Google.Model.Analytics.Factory.Interfaces.pas',
+  Google.Model.Analytics.Factory in '..\..\Source\Model\Google.Model.Analytics.Factory.pas',
+  Google.Model.Analytics.Interfaces in '..\..\Source\Model\Google.Model.Analytics.Interfaces.pas',
+  Google.Model.Analytics.Invoker in '..\..\Source\Model\Google.Model.Analytics.Invoker.pas',
+  Google.Model.Analytics.PageView in '..\..\Source\Model\Google.Model.Analytics.PageView.pas',
+  Google.Model.Analytics.ScreenView in '..\..\Source\Model\Google.Model.Analytics.ScreenView.pas',
+  Google.Model.Analytics.Session in '..\..\Source\Model\Google.Model.Analytics.Session.pas';
 
 {$R *.res}
 
